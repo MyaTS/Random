fdf2b68098b7d4d187a8407a5c5bd8027c58d46f
unknown <myatgsoe@gmail.com>
2019-04-05 14:30:10 -0700

    Add the whole game

---
+++ Space Shooter for the Third Time/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed; 
+    private Rigidbody Rb;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        Rb.velocity = transform.forward * speed;
+    }
+
+}
---
+++ Space Shooter for the Third Time/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    private Rigidbody Rb;
+
+    private void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        Rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+}
